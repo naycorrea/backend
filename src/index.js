4a687e484f3cea792b00c862c03a0612520cb54d
nayaracorrea <43212442+nayaracorrea@users.noreply.github.com>
2020-01-16 18:02:32 -0300

    update

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://nayara:nayara@cluster0-8doqy.mongodb.net/week10?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
+
+//request = requisição
+//response = resposta
+
+// Métodos HTTP: get, post, put, delete
+
+//Tipos de parâmetros:
+//Query params: request.query (Filtros, ordenação, paginação, ...)
+//Route params: request.params (Identificar um recurso na alteração ou remoção)
+//Body: request.body (Dados para criação ou alteração de um registro)
+
+//MongoDB (Não-relacional)
+
